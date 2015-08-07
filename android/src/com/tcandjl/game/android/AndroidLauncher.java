87fd8ef5b78e310779a3b91dc31dd392520b24c3
tjchen856 <litopkboy@gmail.com>
2015-08-06 22:19:35 -0700

    Initial commit

---
+++ android/src/com/tcandjl/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tcandjl.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tcandjl.game.ProjectFrontline;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectFrontline(), config);
+	}
+}
---
+++ desktop/src/com/tcandjl/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcandjl.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcandjl.game.ProjectFrontline;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectFrontline(), config);
+	}
+}
