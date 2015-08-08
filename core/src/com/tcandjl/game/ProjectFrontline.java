f94ae4197b5747015a8fa61f861365353e3d76d8
tjchen856 <litopkboy@gmail.com>
2015-08-07 23:44:27 -0700

    Added Armor sprite. Armor moves left and right on the ground.

--- core/src/com/tcandjl/game/ProjectFrontline.java
+++ core/src/com/tcandjl/game/ProjectFrontline.java
@@ -2,26 +2,99 @@ package com.tcandjl.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tcandjl.game.Mobile.Armor;
+import com.tcandjl.game.Mobile.Mobile;
 
-public class ProjectFrontline extends ApplicationAdapter {
+public class ProjectFrontline extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	Texture img;
+	Mobile armor;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		armor = new Armor();
+
+		armor.getModel().setPosition(armor.getPositionX(), armor.getPositionY());
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
-	public void render () {
+	public void dispose() {
+		batch.dispose();
+	}
+
+	@Override
+	public void render() {
+		float moveAmount = 1.0f;
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			armor.setPositionX(armor.getPositionX() - moveAmount);
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			armor.setPositionX(armor.getPositionX() + moveAmount);
+		}
+
+		armor.getModel().setPosition(armor.getPositionX(), armor.getPositionY());
 		batch.begin();
-		batch.draw(img, 0, 0);
+		armor.getModel().draw(batch);
 		batch.end();
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode == Keys.LEFT) {
+			armor.getModel().flip(true, false);
+		}
+		if(keycode == Keys.RIGHT) {
+			armor.getModel().flip(true, false);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
 	}
 }
--- desktop/src/com/tcandjl/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tcandjl/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.tcandjl.game.ProjectFrontline;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new ProjectFrontline(), config);
 	}
 }
